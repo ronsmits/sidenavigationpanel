c52c7a811754b201b26e5d628b5c74f1eade6589
ronsmits <ron.smits@sgmail.com>
2015-06-03 12:55:13 +0200

    aanpassingen

--- src/main/java/org/ronsmits/navigationpanel/sample/WicketApplication.java
+++ src/main/java/org/ronsmits/navigationpanel/sample/WicketApplication.java
@@ -7,7 +7,6 @@ import org.apache.wicket.protocol.http.WebApplication;
  * Application object for your web application. If you want to run this
  * application without deploying, run the Start class.
  *
- * @see org.ronsmits.wickettest.Start#main(String[])
  */
 public class WicketApplication extends WebApplication {
 
